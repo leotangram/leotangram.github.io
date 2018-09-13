668e5f75a31d4d40fa3aaab75d365f877d31e5cb
Leonardo Omaña <leonardo.omana@imagineapps.co>
2018-09-13 18:39:30 -0500

    Actualizacion Septiembre/13/2018

---
+++ img/Soluciones para nuestra tarea Equi_files/loader-v2.js
@@ -0,0 +1,96 @@
+(function() {
+
+    var __hs_cta_json = {"css":"a#cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b {\n  -webkit-font-smoothing:antialiased !important; \n  cursor:pointer !important; \n  -moz-user-select:none !important; \n  -webkit-user-select:none !important; \n  -o-user-select:none !important; \n  user-select:none !important; \n  display:inline-block !important; \n  font-weight:normal !important; \n  text-align:center !important; \n  text-decoration:none !important; \n  font-family:sans-serif !important; \n  background:rgb(83,205,242) !important; \n  color:rgb(255, 255, 255) !important; \n  border-radius:4px !important; \n  border-width:0px !important; \n  transition:all .4s ease !important; \n  -moz-transition:all .4s ease !important; \n  -webkit-transition:all .4s ease !important; \n  -o-transition:all .4s ease !important; \n  text-shadow:none !important; \n  line-height:1.5em !important; \n  padding:6px 18px !important; \n}\na#cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b:hover {\nbackground:rgb(91,225,255) !important; \ncolor:rgb(255,255,255) !important; \n}\na#cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b:active, #cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b:active:hover {\nbackground:rgb(66,164,193) !important; \ncolor:rgb(244,244,244) !important; \n}\n\n","image_html":"<a id=\"cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b\" class=\"cta_button\" href=\"https://cta-service-cms2.hubspot.com/ctas/v2/public/cs/c/?cta_guid=1f040683-6f1b-4cee-95cf-71c375ffb45b&placement_guid=d40a5ac3-d9e7-4d15-8a28-417dad10547c&portal_id=2502692&redirect_url=APefjpF2lYITI7hrgQdJwFppwdK8sRd5rPSvMRLs21zob4EG6Ue-5aYVtX_wteXZhAMZ70NnRXvujyDlaF86-5ZWAXOa2TCGr7ByPlYyB5r1QX8mjpWk2E7sCTZtnBvdaGK5roeYXYP_7Psgis-iZ74viIfprYRC1I04TOFIDPfHudWx920XE1CEf2QmccCzjVs1iqoGYBWZVDiOUpPC-E1OD4w7kcp4S5xOB00LDyiCoEvS9aHCxac&hsutk=5a26f653d426b6c5e0d16328936f0066&canon=https%3A%2F%2Fblog.codility.com%2F2011%2F03%2Fsolutions-for-task-equi.html&click=b44c9704-35cd-432f-afcc-9c6222deaf40&utm_referrer=https%3A%2F%2Fwww.google.com.co%2F&pageId=4630875780\"  target=\"_blank\"  cta_dest_link=\"https://codility.com/programmers/\"><img id=\"hs-cta-img-d40a5ac3-d9e7-4d15-8a28-417dad10547c\" class=\"hs-cta-img \" style=\"border-width: 0px; /*hs-extra-styles*/\" mce_noresize=\"1\" alt=\"Join our free Programmers&#39; Home\" src=\"https://cdn2.hubspot.net/hubshot/16/12/12/5e48d73f-1426-4683-a89b-950bbf064b8f.png\" /></a>","is_image":false,"placement_element_class":"hs-cta-d40a5ac3-d9e7-4d15-8a28-417dad10547c","raw_html":"<a id=\"cta_button_2502692_1f040683-6f1b-4cee-95cf-71c375ffb45b\" class=\"cta_button \" href=\"https://cta-service-cms2.hubspot.com/ctas/v2/public/cs/c/?cta_guid=1f040683-6f1b-4cee-95cf-71c375ffb45b&placement_guid=d40a5ac3-d9e7-4d15-8a28-417dad10547c&portal_id=2502692&redirect_url=APefjpF2lYITI7hrgQdJwFppwdK8sRd5rPSvMRLs21zob4EG6Ue-5aYVtX_wteXZhAMZ70NnRXvujyDlaF86-5ZWAXOa2TCGr7ByPlYyB5r1QX8mjpWk2E7sCTZtnBvdaGK5roeYXYP_7Psgis-iZ74viIfprYRC1I04TOFIDPfHudWx920XE1CEf2QmccCzjVs1iqoGYBWZVDiOUpPC-E1OD4w7kcp4S5xOB00LDyiCoEvS9aHCxac&hsutk=5a26f653d426b6c5e0d16328936f0066&canon=https%3A%2F%2Fblog.codility.com%2F2011%2F03%2Fsolutions-for-task-equi.html&click=b44c9704-35cd-432f-afcc-9c6222deaf40&utm_referrer=https%3A%2F%2Fwww.google.com.co%2F&pageId=4630875780\" target=\"_blank\" style=\"/*hs-extra-styles*/\" cta_dest_link=\"https://codility.com/programmers/\" title=\"Join our free Programmers&#39; Home\"><span style=\"font-size: 14px; font-family: arial, helvetica, sans-serif;\">Join our free Programmers' Home</span></a>"};
+    var __hs_cta = {};
+
+    __hs_cta.drop = function() {
+        var is_legacy = document.getElementById('hs-cta-ie-element') && true || false,
+            html = __hs_cta_json.image_html,
+            tags = __hs_cta.getTags(),
+            is_image = __hs_cta_json.is_image,
+            parent, _style;
+
+        if (!is_legacy && !is_image) {
+            parent = (document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]);
+
+            _style = document.createElement('style');
+            parent.insertBefore(_style, parent.childNodes[0]);
+            try {
+                default_css = ".hs-cta-wrapper p, .hs-cta-wrapper div { margin: 0; padding: 0; }";
+                cta_css = default_css + " " + __hs_cta_json.css;
+                // http://blog.coderlab.us/2005/09/22/using-the-innertext-property-with-firefox/
+                _style[document.all ? 'innerText' : 'textContent'] = cta_css;
+            } catch (e) {
+                // addressing an ie9 issue
+                _style.styleSheet.cssText = cta_css;
+            }
+
+            html = __hs_cta_json.raw_html;
+        }
+
+        for (var i = 0; i < tags.length; ++i) {
+
+            var tag = tags[i];
+            var images = tag.getElementsByTagName('img');
+            var cssText = "";
+            var align = "";
+            for (var j = 0; j < images.length; j++) {
+                align = images[j].align;
+                images[j].style.border = '';
+                images[j].style.display = '';
+                cssText = images[j].style.cssText;
+            }
+
+            if (align == "right") {
+                tag.style.display = "block";
+                tag.style.textAlign = "right";
+            } else if (align == "middle") {
+                tag.style.display = "block";
+                tag.style.textAlign = "center";
+            }
+
+            tag.innerHTML = html.replace('/*hs-extra-styles*/', cssText);
+            tag.style.visibility = 'visible';
+            tag.setAttribute('data-hs-drop', 'true');
+            window.hbspt && hbspt.cta && hbspt.cta.afterLoad && hbspt.cta.afterLoad('d40a5ac3-d9e7-4d15-8a28-417dad10547c');
+        }
+
+        return tags;
+    };
+
+    __hs_cta.getTags = function() {
+        var allTags, check, i, divTags, spanTags,
+            tags = [];
+            if (document.getElementsByClassName) {
+                allTags = document.getElementsByClassName(__hs_cta_json.placement_element_class);
+                check = function(ele) {
+                    return (ele.nodeName == 'DIV' || ele.nodeName == 'SPAN') && (!ele.getAttribute('data-hs-drop'));
+                };
+            } else {
+                allTags = [];
+                divTags = document.getElementsByTagName("div");
+                spanTags = document.getElementsByTagName("span");
+                for (i = 0; i < spanTags.length; i++) {
+                    allTags.push(spanTags[i]);
+                }
+                for (i = 0; i < divTags.length; i++) {
+                    allTags.push(divTags[i]);
+                }
+
+                check = function(ele) {
+                    return (ele.className.indexOf(__hs_cta_json.placement_element_class) > -1) && (!ele.getAttribute('data-hs-drop'));
+                };
+            }
+            for (i = 0; i < allTags.length; ++i) {
+                if (check(allTags[i])) {
+                    tags.push(allTags[i]);
+                }
+            }
+        return tags;
+    };
+
+    // need to do a slight timeout so IE has time to react
+    setTimeout(__hs_cta.drop, 10);
+    window._hsq = window._hsq || [];
+    window._hsq.push(['trackCtaView', 'd40a5ac3-d9e7-4d15-8a28-417dad10547c', '1f040683-6f1b-4cee-95cf-71c375ffb45b']);
+}());
